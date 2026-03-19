62b494b22fb793ccb10acf519edc690c0ad69331
hrustinszkiadam <hrustinszkiadam@gmail.com>
2026-03-19 09:28:05 +0100

    feat: add UserRole

--- packages/db/src/schema/auth.schema.ts
+++ packages/db/src/schema/auth.schema.ts
@@ -1,5 +1,10 @@
 import { relations } from 'drizzle-orm';
+import { pgEnum } from 'drizzle-orm/pg-core';
 import { pgTable, text, timestamp, boolean, index } from 'drizzle-orm/pg-core';
+
+export const userRoles = ['user', 'admin'] as const;
+export type UserRole = (typeof userRoles)[number];
+export const userRoleEnum = pgEnum('user_role', userRoles);
 
 export const users = pgTable('users', {
   id: text('id').primaryKey(),
@@ -7,6 +12,7 @@ export const users = pgTable('users', {
   email: text('email').notNull().unique(),
   emailVerified: boolean('email_verified').default(false).notNull(),
   image: text('image'),
+  role: userRoleEnum('role').default('user').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at')
     .defaultNow()
